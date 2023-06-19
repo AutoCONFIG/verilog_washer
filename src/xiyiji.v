c6b945513fdb44f161226571e1160c9366b59338
LALA <1559272797@qq.com>
2023-06-19 17:07:42 +0800

    mode: fix mode 2 functions

--- src/xiyiji.v
+++ src/xiyiji.v
@@ -1,18 +1,24 @@
-module xiyiji(select,ledzheng,ledfan,ledstop,clk,zheng,fan,start,alarm,emergency,count,rst);
+module xiyiji(select,ledzheng,ledfan,ledstop,ledinlet,leddrain,leddry,clk,zheng,fan,mode_c,inlet,drain,dry,start,alarm,emergency,count,rst,     c_s,time_c,enable,tmp);
 input select,clk,start,emergency,rst;  //#select：选择工作模式，#clk：秒时钟，#start：启动洗衣机，#emergency：急停模式，#rst：复位
-output ledfan,ledzheng,ledstop,ledinlet,leddrain,zheng,fan,alarm; //#ledfan：反转指示灯 #ledzheng：正转指示灯 #ledinlet：进水指示灯 #leddrain：排水指示灯 #ledstop：待机指示灯    #zheng/fan：正/反转输出 #alarm：警报    #count：计数剩余值
-output count,mode,inlet,drain;//循环计数器，工作模式（洗、漂洗、脱水），进水，排水
+output ledfan,ledzheng,ledstop,ledinlet,leddrain,leddry,zheng,fan,alarm; //#ledfan：反转指示灯 #ledzheng：正转指示灯 #ledinlet：进水指示灯 #leddrain：排水指示灯 #ledstop：待机指示灯 #zheng/fan：正/反转输出 #alarm：警报    #count：计数剩余值
+output count,mode_c,inlet,drain,dry;//循环计数器，工作模式（洗、漂洗、脱水），进水，排水
+output c_s,time_c,enable,tmp; //调试用
 
-reg ledfan,ledstop,ledzheng,ledinlet,leddrain,zheng,fan,alarm;
-reg [1:0] mode; //mode[0]：未指定，mode[1]：洗，mode[2]：漂洗，mode[3]：脱水
-reg [1:0] c_s,n_s; //模式状态机，电机状态机
+reg ledfan,ledstop,ledzheng,ledinlet,leddrain,leddry,zheng,fan,inlet,drain,dry,alarm;
+reg enable;
+reg [1:0] tmp;
+reg [1:0] mode_t,mode_c; //mode[0]：未指定，mode[1]：洗，mode[2]：漂洗，mode[3]：脱水
+reg [3:0] c_s,n_s; //模式状态机，电机状态机
 reg [3:0] time_t,time_c;
 reg [5:0] count;
 
-parameter S0 = 2'b00;   //待机状态
-parameter S1 = 2'b01;   //正转状态
-parameter S2 = 2'b10;   //待机状态
-parameter S3 = 2'b11;   //反转状态
+parameter S0 = 4'b0000;   //待机状态
+parameter S1 = 4'b0001;   //正转状态
+parameter S2 = 4'b0010;   //待机状态
+parameter S3 = 4'b0011;   //反转状态
+parameter S4 = 4'b0100;   //进水状态
+parameter S5 = 4'b0101;   //排水状态
+parameter S6 = 4'b0110;   //脱水状态
 
 parameter M0 = 2'b00;   //模式0，待机
 parameter M1 = 2'b01;   //模式1，漂洗
@@ -20,21 +26,27 @@ parameter M2 = 2'b10;   //模式2，洗涤
 parameter M3 = 2'b11;   //模式3，脱水
 
 always @(negedge select) begin
-    if(!select && mode == M3) begin
-        mode <= M0;
+    if(!select && mode_t == M3) begin
+        mode_t <= M0;
     end
     if (!select) begin
-        mode <= mode + 2'b01;
+        mode_t <= mode_t + 2'b01;
     end
 end
 
 always@(negedge emergency) begin
-    if (!emergency) begin //紧急状态
+    if (emergency == 0) begin //紧急状态
+        inlet <= 0;
+        drain <= 0;
+        dry <= 0;
         zheng <= 0;
         fan <= 0;
-        ledstop <= 1;
+        ledinlet <= 0;
+        leddrain <= 0;
+        leddry <= 0;
         ledzheng <= 0;
         ledfan <= 0;
+        ledstop <= 1;
     end
 end
 
@@ -45,23 +57,38 @@ always@(posedge clk or negedge rst) begin   //D触发器模块
         c_s <= n_s;
 end
 
-always @(mode or clk) begin
-    case (mode)
+always @(mode_c or time_c) begin
+    case (mode_c)
         M0:begin //未指定，意味待机状态
             time_t <= 4'b0000;
         end
-        M1:begin //漂洗，电机循环15次
+        M1:begin //漂洗，电机状态循环15次
             time_t <= 4'b1111;
         end
         M2:begin //洗涤（洗，漂洗，脱水）
-            time_t <= 4'b0111;  //洗，电机循环7次
-            while (time_c); //洗
-            time_t <= 4'b1111;  //漂洗，电机循环15次
-            mode <= M3;
+            time_t <= 4'b0111;  //洗，电机状态循环7次
+            if (time_c == 4'b0000 && tmp == 0) begin
+                time_t <= 4'b1111;
+                enable <= 0;
+                tmp <= 1;
+            end
+            if (time_c == 4'b0000 && tmp == 1) begin
+                n_s <= S5;
+                time_t <= 4'b0000; //洗循环清零
+                enable <= 0;
+                tmp <= 2;
+            end
+            if (tmp == 2) begin
+                time_t <= 4'b0000; //洗循环清零
+                enable <= 0;
+                tmp <=3;
+            end
         end
         M3:begin    //脱水
-            n_s <= S5
+            n_s <= S5;
             time_t <= 4'b0000; //洗循环清零
+            enable <= 0;
+            tmp <=3;
         end
     endcase
 end
@@ -116,22 +143,33 @@ always @(count or c_s) begin    //n_s模块
         end
         default:begin
             n_s <= S0;
+            mode_t <= 2'b00;
             time_t <= 4'b0000;
         end
     endcase
 end
 
-always@(posedge clk) begin   //c_s模块
-    if (!start) begin   //未按下启动建，始终保持初始化状态
-        n_s <= S0;
-        mode <= 2'b00;
-        count <= 6'b000000;
+always@(posedge clk or negedge enable) begin
+    if (!enable) begin   //刷新状态使能
         time_c <= time_t;
-        alarm  <= 0;
+        mode_c <= mode_t;
+        enable <= 1;
     end
-    else if (time_c == 0) begin //洗衣流程结束
+end
+
+always@(posedge clk) begin   //c_s模块
+    if (start == 0) begin   //未按下启动建，始终保持初始化状态
         n_s <= S0;
-        mode <= 2'b00;
+        count <= 6'b000000;
+        tmp <= 0;
+        time_c <= time_t;
+        mode_c <= mode_t;
+        alarm  <= 0;
+        enable <= 1;
+    end
+    else if (time_c == 0 && tmp == 3) begin //洗衣流程结束
+        n_s <= S0;
+        mode_t <= 2'b00;
         count <= 6'b000000;
         alarm <= 1;
     end
@@ -141,7 +179,7 @@ always@(posedge clk) begin   //c_s模块
         S0:begin
             inlet <= 0;
             drain <= 0;
-            dry <=0;
+            dry <= 0;
             zheng <= 0;
             fan <=0;
             ledinlet <= 0;
--- src/xiyiji_tb.v
+++ src/xiyiji_tb.v
@@ -1,55 +1,34 @@
-`timescale 1ms/1ms
+`timescale 100ms/1ms
 
-module xiyiji_tb(add,clk,start,emergency,rst);
-    output reg clk,start,emergency,add,rst;
+module xiyiji_tb(select,clk,start,emergency,rst);
+    output reg clk,start,emergency,select,rst;
 
-    wire ledzheng,ledfan,ledstop,zheng,fan,alarm;
+    wire ledzheng,ledfan,ledstop,ledinlet,leddrain,leddry,zheng,fan,inlet,drain,dry,alarm;
     
+    wire [1:0] mode_c;
     wire [5:0] count;
+
+    wire enable;
+    wire [1:0] tmp;
+    wire [3:0] c_s,time_c; //Debug
 
     initial begin
         clk=0;
-        add=0;
+        select=0;
         start=0;
         rst=1;
         emergency=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
-        #100 add=1;
-        #100 add=0;
+        #100 select=1;
+        #100 select=0;
+        #100 select=1;
+        #100 select=0;
+        #100 select=1;
+        #100 select=0;
         #100 start=1;
     end
 
-    always #10 clk=~clk;
+    always #5 clk=~clk;
 
-xiyiji M8(.add(add), .ledzheng(ledzheng), .ledfan(ledfan), .ledstop(ledstop), .clk(clk), .zheng(zheng), .fan(fan), .start(start), .alarm(alarm), .emergency(emergency), .count(count), .rst(rst));
+xiyiji M8(.select(select), .ledzheng(ledzheng), .ledfan(ledfan), .ledstop(ledstop), .ledinlet(ledinlet), .leddrain(leddrain), .leddry(leddry), .clk(clk), .zheng(zheng), .fan(fan), .mode_c(mode_c), .inlet(inlet), .drain(drain), .dry(dry), .start(start), .alarm(alarm), .emergency(emergency), .count(count), .rst(rst), .c_s(c_s), .time_c(time_c), .enable(enable), .tmp(tmp));
 
 endmodule
